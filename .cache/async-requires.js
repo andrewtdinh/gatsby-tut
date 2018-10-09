857356a142b54ac430d39f4a2591363e70ce6cde
Andrew Dinh <adinh@sightmachine.com>
2018-10-09 09:16:57 -0700

    Create another page component and sub-component

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,7 +3,8 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("/Users/andrewd/Personal/gatsby-tut/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-index-js": () => import("/Users/andrewd/Personal/gatsby-tut/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+  "component---src-pages-index-js": () => import("/Users/andrewd/Personal/gatsby-tut/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-about-js": () => import("/Users/andrewd/Personal/gatsby-tut/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */)
 }
 
 exports.data = () => import("/Users/andrewd/Personal/gatsby-tut/.cache/data.json")
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default () => (
-  <div style={{color: `purple`, fontSize: `36px`}}>Hello Gatsby!</div>
+  <div style={{color: `purple`}}>
+    <h2>Hello Gatsby!</h2>
+    <p>What a world</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
 )
