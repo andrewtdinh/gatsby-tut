d3627546314ae0cbb18612b403321abb22586e36
Andrew Dinh <adinh@sightmachine.com>
2018-10-09 00:34:40 -0700

    Chang font size and color

---
+++ .cache/sync-requires.js
@@ -0,0 +1,9 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/andrewd/Personal/gatsby-tut/.cache/dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/andrewd/Personal/gatsby-tut/src/pages/index.js"))
+}
+
