199e0e866d7b520ccf795a44f05a9b2c39e680a7
Andrew Dinh <adinh@sightmachine.com>
2018-10-09 22:40:51 -0700

    Add Contact link on Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
-import React from "react"
+import React from 'react';
+import { Link } from 'gatsby';
+import Header from '../components/header';
 
 export default () => (
   <div style={{color: `purple`}}>
-    <h2>Hello Gatsby!</h2>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText='Hello Gatsby' />
     <p>What a world</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
