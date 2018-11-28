884504a7440715a53bb9e1e35dc96c64ccb6aac8
Andrew Dinh <adinh@sightmachine.com>
2018-11-28 09:38:59 -0800

    Use layout component on about and contact pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,10 @@
 import React from "react"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default () => (
-  <div style={{ color: `teal` }}>
+  <Layout style={{ color: `teal` }}>
     <Header headerText="About me" />
     <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
-  </div>
+  </Layout>
 )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Header from '../components/header';
+import Layout from "../components/layout"
 
 export default () => (
-  <div style={{color: `teal`}}>
+  <Layout style={{color: `teal`}}>
     <Link to='/'>Home</Link>
     <Header headerText="I'd love to talk! Email me at the address below" />
     <p>
       <a href="mailto:me@example.com">me@example.com</a>
     </p>
-  </div>
+  </Layout>
 )
