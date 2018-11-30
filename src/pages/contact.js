f479d86c31b2baf4e6fe6550a66d833e36b0246e
Andrew Dinh <adinh@sightmachine.com>
2018-11-29 21:56:58 -0800

    Change layout component to include links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Header from '../components/header';
 import Layout from "../components/layout"
 
 export default () => (
   <Layout style={{color: `teal`}}>
-    <Link to='/'>Home</Link>
     <Header headerText="I'd love to talk! Email me at the address below" />
     <p>
       <a href="mailto:me@example.com">me@example.com</a>
